08e962907f2dd16abdd7ebdc0952e56372d57823
Kendall Wahnschaffe <kpwahn@live.com>
2020-03-09 08:15:52 -0500

    updating things

---
+++ build/precache-manifest.4fe2491cc3695c96e6e33f421ba77919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beaad9b8a76db4cd4ea746fbbe0b6da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c2466bfa8c477843935",
+    "url": "/static/css/main.0137cbff.chunk.css"
+  },
+  {
+    "revision": "7959760d0b2fe1c968c4",
+    "url": "/static/js/2.299abcc7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.299abcc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2466bfa8c477843935",
+    "url": "/static/js/main.a27ece26.chunk.js"
+  },
+  {
+    "revision": "bd070fb5a6b139470575",
+    "url": "/static/js/runtime-main.637db5b6.js"
+  }
+]);
